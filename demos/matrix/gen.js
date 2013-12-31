8157d2bab5bc0d8eac36708e4d4cbc6002a7fac6
James Nutt <james.srn@gmail.com>
2013-12-31 23:16:37 +0000

    matrix

---
+++ demos/matrix/gen.js
@@ -0,0 +1,98 @@
+String.prototype.replaceAt=function(index, character) {
+	return this.substr(0, index) + character + this.substr(index+character.length);
+}
+
+chars = "ABCDEFGHIJKLMNOPQRSTUVQXYZabcdefghijklmnopqrstuvqxyz0123456789"
+
+heartBlock =  "------------------------------------------------------------"
+heartBlock += "------------------------------------------------------------"
+heartBlock += "-----------------@@@@@@@------------@@@@@@@-----------------"
+heartBlock += "---------------@@@@@@@@@@@--------@@@@@@@@@@@---------------"
+heartBlock += "--------------@@@@@@@@@@@@@@----@@@@@@@@@@@@@@--------------"
+heartBlock += "--------------@@@@@@@@@@@@@@@--@@@@@@@@@@@@@@@--------------"
+heartBlock += "--------------@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@--------------"
+heartBlock += "---------------@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@---------------"
+heartBlock += "----------------@@@@@@@@@@@@@@@@@@@@@@@@@@@@----------------"
+heartBlock += "-----------------@@@@@@@@@@@@@@@@@@@@@@@@@@-----------------"
+heartBlock += "------------------@@@@@@@@@@@@@@@@@@@@@@@@------------------"
+heartBlock += "-------------------@@@@@@@@@@@@@@@@@@@@@@-------------------"
+heartBlock += "--------------------@@@@@@@@@@@@@@@@@@@@--------------------"
+heartBlock += "---------------------@@@@@@@@@@@@@@@@@@---------------------"
+heartBlock += "----------------------@@@@@@@@@@@@@@@@----------------------"
+heartBlock += "------------------------@@@@@@@@@@@@------------------------"
+heartBlock += "-------------------------@@@@@@@@@@-------------------------"
+heartBlock += "---------------------------@@@@@@---------------------------"
+heartBlock += "-----------------------------@@-----------------------------"
+heartBlock += "------------------------------------------------------------";
+
+hashIndices = [];
+count = 0;
+
+function init()
+{
+	for(var i = 0; i < heartBlock.length; i++)
+	{
+		var chr = heartBlock[i];
+		if( chr === "@")
+		{
+			hashIndices.push(i);
+		}
+	}
+}
+
+function getBlock()
+{
+	var block = getBackgroundBlock();
+	block = overlayHashes(block);
+	block = addBreaks(block);
+	return block;
+}
+
+function getBackgroundBlock()
+{
+	var block = ""
+
+	for(var i = 0; i < 20; i++)
+	{
+		for(var j = 0; j < 60; j++)
+		{
+			block += chars.charAt(Math.floor(Math.random() * chars.length));;
+		}
+	}
+
+	return block;
+}
+
+function overlayHashes( block )
+{
+	for(var i = 0; i < hashIndices.length; i++)
+	{
+		var chance = count / hashIndices.length;
+		if( Math.random() < chance )
+		{
+			var ind = hashIndices[i];
+			block = block.replaceAt(ind,"@");	
+		}
+	}
+
+	return block;
+}
+
+function addBreaks( block )
+{
+	var parts = block.match(/.{1,60}/g);
+	return parts.join("<br>");
+}
+
+function updateBlock()
+{
+	var displayBlock = getBlock();
+	document.getElementById("block").innerHTML = displayBlock;
+	count++;
+}
+
+init();
+
+updateBlock();
+
+setInterval(updateBlock, 100);
